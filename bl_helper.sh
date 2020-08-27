249d09714019390814b268aaa3455eab4c7ddb70
Chris B <chrisb@cereproc.com>
2020-08-27 14:38:15 +0100

    resonance: third-party cloning and api built

--- bl_helper.sh
+++ bl_helper.sh
@@ -6,3 +6,21 @@ git config --global alias.update '!git pull && git submodule update --init --rec
 
 # update everything (alias set up)
 git update
+
+
+# --------------------------------------------
+# --------- Google Resonance Audio -----------
+# --------------------------------------------
+
+git submodule add https://github.com/resonance-audio/resonance-audio.git resonance
+
+# build/clone third-party deps
+cd /home/chrisb/binaural-league/resonance/third_party
+./clone_core_deps.sh
+
+# build the API
+cd ..
+./build.sh -t=RESONANCE_AUDIO_API
+
+# run unit tests
+./build.sh -t=RESONANCE_AUDIO_TESTS
